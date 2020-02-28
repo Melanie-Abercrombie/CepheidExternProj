71f9c1be3ac723f88fb375d42c71b2ab08af4dcf
emdang <43802868+emdang@users.noreply.github.com>
2020-02-27 17:47:19 -0800

    Changed Build Settings to Android

---
+++ CepheidVR/Assets/openCloseBox.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openCloseBox : MonoBehaviour
+{
+    bool opened=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void  OnTriggerEnter(Collider other) {
+        Debug.Log("collided");
+        if(other.gameObject.tag=="Player"){
+            if(!opened){
+                Debug.Log("open");
+                opened=true;
+            }
+            else{
+                Debug.Log("close");
+                opened=false;
+            }
+        }
+    }
+}
